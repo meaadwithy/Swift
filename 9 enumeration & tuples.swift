4dce668d918346eb89ef01e3726fe3e1bf608bc3
Meaadwithy <meaad.alqthmi145@gmail.com>
2026-06-21 23:19:03 +0300

    Define basic enum for clothing sizes
    Added a basic enumeration for clothing sizes.

--- 9 enumeration & tuples.swift
+++ 9 enumeration & tuples.swift
@@ -1,1 +1,13 @@
 
+/*enumeation three types we ca use for the (sunday and monday bc thye are related for being in week)
+* basic enum
+* enum with raw values
+* enum with associated values
+*/
+enum size {
+  case XS
+  case S
+  case M
+  case L
+  case XL
+}
