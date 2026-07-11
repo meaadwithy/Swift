876d702fda9cfdec28b711a87c93e6471dd7530a
Meaadwithy <meaad.alqthmi145@gmail.com>
2026-07-11 15:31:52 +0300

    Revise error types and handles comments
    Updated error types documentation for clarity.

--- 94 Error.swift
+++ 94 Error.swift
@@ -1,2 +1,9 @@
 
-// error types and handles 
+// Error and handles 
+/*
+*Error types*
+Syntax Error  / user syntax incorrect.
+Runtime Error / crashes while running.
+Logic Error   / crashes while running.
+Recoverable Errors / internet problem.
+*/
