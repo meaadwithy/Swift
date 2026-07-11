45e023ba612b37fd155ce43268a3ecbd069491bc
Meaadwithy <meaad.alqthmi145@gmail.com>
2026-07-11 16:12:43 +0300

    Fix spelling and improve error handling in password length
    Renamed function from 'passLenght' to 'passLength' and updated its usage. Added error handling examples for password length validation.

--- 94 Error.swift
+++ 94 Error.swift
@@ -21,15 +21,34 @@ enum LengthError : Error {
     case maxLength
     case minLength
 }
-func passLenght(lenght: Int) throws -> String{
-    if (lenght < 7){
+func passLength(length: Int) throws -> String{
+    if (length < 7){
         print("length is less than 7.")
         throw LengthError.minLength
-    }else if(lenght > 15){
+    }else if(length > 15){
         print("length is greater than 15.")
         throw LengthError.maxLength
     }
     return "length is valid"
 }
-let passwordLength = try? passLenght(lenght: 6)
+// there is many why to handle error
+//1
+let passwordLength = try? passLength(length: 6)
 print(passwordLength)
+//2
+do {
+    let passwordLength = try passLength(length:17)
+    print(passwordLength)}
+catch{
+    print(error)
+}
+//3
+do {
+    let passwordLength = try passLength(length:17)
+    print(passwordLength)
+}catch LengthError.maxLength{
+    print("reduse length of password to be valid")
+}
+catch{
+    print(error)
+}
