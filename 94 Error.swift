ba03d3f207bde0335284c3175b9c64879cd0b402
Meaadwithy <meaad.alqthmi145@gmail.com>
2026-07-11 16:05:26 +0300

    Implement password length validation with errors
    Added LengthError enum and passLenght function to validate password length.

--- 94 Error.swift
+++ 94 Error.swift
@@ -16,3 +16,20 @@ Runtime Error / crashes while running.
 Logic Error   / crashes while running.
 Recoverable Errors / internet problem.
 */
+// let say we want password lenght between 7 & 15 so any other number eill be Error
+enum LengthError : Error {
+    case maxLength
+    case minLength
+}
+func passLenght(lenght: Int) throws -> String{
+    if (lenght < 7){
+        print("length is less than 7.")
+        throw LengthError.minLength
+    }else if(lenght > 15){
+        print("length is greater than 15.")
+        throw LengthError.maxLength
+    }
+    return "length is valid"
+}
+let passwordLength = try? passLenght(lenght: 6)
+print(passwordLength)
