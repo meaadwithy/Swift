898b8ba575b247bc941322c23d00f7b0f983c94a
Meaadwithy <meaad.alqthmi145@gmail.com>
2026-07-12 22:50:46 +0300

    Implement KitchenDelegate for cooking notifications
    Added KitchenDelegate protocol and implemented cooking functionality.

--- 95 Protocol.swift
+++ 95 Protocol.swift
@@ -24,3 +24,30 @@ let plane = Plane()
 plane.Fly()
 let bird = Bird()
 bird.Fly()
+
+
+// protocol with delegate.
+protocol KitchenDelegate {
+    func didCookFood(foodName: String)
+}
+class Kitchen{
+    var delegate : KitchenDelegate?
+    func cook(food: String){
+        delegate?.didCookFood(foodName: food)
+    }
+}
+class Waiter {
+    func didCookFood(foodName: String){
+        print("\(foodName)is ready")
+    }
+    var kitchen : Kitchen
+    init(kitchen: Kitchen) {
+        self.kitchen = kitchen
+    }
+    func sendOrderToKitchen(foodName: String){
+        kitchen.cook(food: foodName)
+    }
+}
+let kitchen = Kitchen()
+let waiter = Waiter(kitchen: kitchen)
+waiter.sendOrderToKitchen(foodName: "pasta")
