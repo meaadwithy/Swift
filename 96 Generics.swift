e8312e957b205bd63336a044e6970d2e96bb65e5
Meaadwithy <meaad.alqthmi145@gmail.com>
2026-07-13 01:03:35 +0300

    Implement integer comparison function
    Added a function to compare two integers and print the result.

--- 96 Generics.swift
+++ 96 Generics.swift
@@ -1,2 +1,13 @@
 
+
+//gruad
+func compare (num1:Int , num2:Int){
+    guard num1 > num2 else {
+        print("\(num2) is greater (false)")
+        return
+    }
+    print("\(num1) is greater (true)")
+}
+
+compare(num1: 10, num2: 2)
 // generic
