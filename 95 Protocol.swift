7b62619e09819e8f7ae75e68edcd71ae8f64bfba
Meaadwithy <meaad.alqthmi145@gmail.com>
2026-07-12 20:50:46 +0300

    Add Flyable protocol and implementations for Plane and Bird

--- 95 Protocol.swift
+++ 95 Protocol.swift
@@ -7,3 +7,20 @@ Codable
 Equatable
 Error
 */
+protocol Flyable{
+    func Fly()
+}
+struct Plane : Flyable{
+    func Fly(){
+        print("plane is flying")
+    }
+}
+struct Bird : Flyable{
+    func Fly(){
+        print("bird is flying")
+    }
+}
+let plane = Plane()
+plane.Fly()
+let bird = Bird()
+bird.Fly()
