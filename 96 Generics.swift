cce0128c873a084638bba72e79ff2a54dc628701
Meaadwithy <meaad.alqthmi145@gmail.com>
2026-07-13 15:57:17 +0300

    Implement generic function to swap values
    Added a generic function to swap two values of any type.

--- 96 Generics.swift
+++ 96 Generics.swift
@@ -11,4 +11,20 @@ func compare (num1:Int , num2:Int){
 
 compare(num1: 10, num2: 2)
 
-// generic its overloading func 
+// generic its overloading func and swap
+
+func swapTwoValues<myDataType>( a: inout myDataType , b: inout myDataType) {
+    let temp = a
+    a = b
+    b = temp
+        
+    print("a= \(a)  b=\(b)")
+}
+var someValue = 3
+var anotherValue = 7
+swapTwoValues(a: &someValue, b: &anotherValue)
+/*
+var someValue = "tree"
+var anotherValue = "sun"
+swapTwoValues(a: &someValue, b: &anotherValue)
+*/
