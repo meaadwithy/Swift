54335f3d1ddb47bd3c58ff6a14a99d3185cfe46f
Meaadwithy <meaad.alqthmi145@gmail.com>
2026-07-13 14:42:07 +0300

    Improve comments in 96 Generics.swift
    Updated comments for clarity and corrected formatting.

--- 96 Generics.swift
+++ 96 Generics.swift
@@ -1,6 +1,6 @@
 
 
-//gruad
+//gruad like if when its false stop the loop and excute the false statment
 func compare (num1:Int , num2:Int){
     guard num1 > num2 else {
         print("\(num2) is greater (false)")
@@ -10,4 +10,5 @@ func compare (num1:Int , num2:Int){
 }
 
 compare(num1: 10, num2: 2)
-// generic
+
+// generic its overloading func 
